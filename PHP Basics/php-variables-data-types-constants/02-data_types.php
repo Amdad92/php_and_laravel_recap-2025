5d77abbb189b09ed5418419181fd7aee3d9fc69f
Amdad92 <emdadctg92@gmail.com>
2025-01-08 22:46:38 +0600

    explore different data types in PHP

---
+++ PHP Basics/php-variables-data-types-constants/02-data_types.php
@@ -0,0 +1,15 @@
+<?php 
+    /*
+        Data Types in PHP:
+            1. Integer      //Integers are whole numbers without decimals, which can be positive or negative.
+            2. Doubles      // Floats are numbers with decimal points
+            3. NULL         //NULL data type represents a variable with no value
+            4.Strings       //text type data
+            5.Booleans      //It return true or false
+            6.Array         //It's a collection of same data types
+            7.Object        //custom data type which create from classes
+            8.Resources     //fclose,fopen
+    */
+
+
+?>
