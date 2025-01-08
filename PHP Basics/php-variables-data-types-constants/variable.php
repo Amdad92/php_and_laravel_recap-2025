185fcda8553b8a709a2cba672eb432aaceeb9449
Amdad92 <emdadctg92@gmail.com>
2025-01-08 22:15:13 +0600

    study php variables and variable rules

---
+++ PHP Basics/php-variables-data-types-constants/variable.php
@@ -0,0 +1,38 @@
+<?php 
+    //string 
+
+    $name="Amdadul Islam"; 
+    echo ($name);
+    
+    // integer
+    $_age=32;
+    echo($_age.PHP_EOL);
+
+    //float
+    $salary=35000.50;
+    echo($salary.PHP_EOL); //
+
+    //Boolean
+    $isEmployed=true;
+    echo($isEmployed);
+
+    /*
+    rules of naming variable in PHP:
+
+        ********************************
+        1.Start with a Letter or Underscore
+        2.Use Descriptive Names  e.g. $userName, $totalAmount.
+        3.PHP variable names are case-sensitive, meaning $name and $Name are different variables.
+        4.Do not use PHP reserved words or keywords as variable names (e.g., function, class, echo).
+        *******************************
+        
+            $firstName = "Alice";   // Valid
+            $_age = 25;             // Valid
+            
+            $2ndPlace = "Bob";      // Invalid: Cannot start with a number
+            $class = "Physics";     // Valid, but avoid reserved words
+   */ 
+
+
+    
+?>
