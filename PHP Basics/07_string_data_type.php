7a95fbbe219ab43218badd787b4a7c13ab3991ca
Amdad92 <emdadctg92@gmail.com>
2025-01-17 10:10:15 +0600

    explore string and boolean data type

---
+++ PHP Basics/07_string_data_type.php
@@ -0,0 +1,9 @@
+<?php 
+    $name="Amdadul Islam";
+    echo "The name of the PHP learner is: $name".PHP_EOL;
+    var_dump($name);
+
+    /*
+    Hold letters or any alphabets, even numbers are included. These are written within double quotes during declaration.
+    */
+?>
---
+++ PHP Basics/08_Boolean_data_types.php
@@ -0,0 +1,16 @@
+<?php 
+    if(true)
+    echo "This is true";
+    if(false)
+    echo "This condition is not true."
+    
+    /*
+        Boolean Data Types in Conditional Testing:
+
+            - Can hold only two values: TRUE (1) or FALSE (0).
+            - True is returned for successful events, and false for unsuccessful events.
+            - NULL and 0 are treated as false in Boolean.
+            - An empty string ("") is also considered false in Boolean.
+            
+    */
+?>
