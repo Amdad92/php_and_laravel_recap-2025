a821e7353de2d548e82472b905e8ab491b9c0f58
Amdad92 <emdadctg92@gmail.comgit config --global user.email>
2025-01-20 10:23:09 +0530

    Explore Important Built-in String Functions

--- PHP Basics/07_string_data_type.php
+++ PHP Basics/07_string_data_type.php
@@ -7,4 +7,20 @@
         - Hold letters or any alphabets, even numbers are included. 
         - These are written within double quotes during declaration.
     */
+
+    /* Some Important Built-in String Functions
+
+        1. PHP strlen() Function - This function is used to find the length of a string.
+        2. PHP strrev() Function - This function is used to reverse a string.
+        3. PHP str_replace() Function - 
+
+    */
+    echo strlen("Hello Amdadul Islam");
+
+    echo strrev("Hello Amdad!");
+
+    /*
+    PHP str_replace() Function -
+
+    */
 ?>
