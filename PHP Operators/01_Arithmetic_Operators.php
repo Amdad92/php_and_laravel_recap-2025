9f4f3e311c90ada17ad403614540d3ec3eda631c
Amdad92 <emdadctg92@gmail.comgit config --global user.email>
2025-01-20 15:30:09 +0530

    explore arithmetic operator

---
+++ PHP Operators/01_Arithmetic_Operators.php
@@ -0,0 +1,24 @@
+<?php 
+// Define two numbers
+$x = 15;
+$y = 5;
+
+// Addition
+echo "Addition: " . ($x + $y) . "\n";
+
+// Subtraction
+echo "Subtraction: " . ($x - $y) . "\n";
+
+// Multiplication
+echo "Multiplication: " . ($x * $y) . "\n";
+
+// Division
+echo "Division: " . ($x / $y) . "\n";
+
+// Exponentiation
+echo "Exponentiation: " . ($x ** $y) . "\n"; // $x raised to the power of $y
+
+// Modulus
+echo "Modulus: " . ($x % $y) . "\n";
+
+?>
