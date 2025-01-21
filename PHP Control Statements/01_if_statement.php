ce244ca1f1b0f23f3c0bc1062b1902c7998a6bbf
Amdad92 <emdadctg92@gmail.comgit config --global user.email>
2025-01-21 12:41:20 +0530

    explore if statement

---
+++ PHP Control Statements/01_if_statement.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+   if statements:
+    
+        syntax: if (condition)
+        {
+            if TRUE then execute this code
+        }
+*/
+    $age=18;
+    if($age>=18){
+        echo "You are an adult";
+    }
+
+    $x =12; 
+    if ($x > 0) { 
+        echo "\nThe number is positive"; 
+    } 
+?>
