8186a5e8fff1945c46777522a1076331c2006326
Amdad92 <emdadctg92@gmail.com>
2025-01-21 19:14:19 +0600

    explore while loop

---
+++ PHP Loops/02_while_loop.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+    PHP while Loop:
+        The while loop in PHP is a control flow statement that allows code to execute repeatedly based on a given condition. The loop will continue running as long as the specified condition evaluates to true.
+        Syntax:
+            while (condition) {
+             Code to be executed
+            }
+*/
+    //  print number 1 to 5.
+        $counter=1;
+        while($counter<=5){
+            echo "$counter\n";
+            $counter++;
+        }
+
+?>
