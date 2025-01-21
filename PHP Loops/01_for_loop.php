34b49dcf0867e59b9116860b4a0fdf36ab3a93ea
Amdad92 <emdadctg92@gmail.com>
2025-01-21 18:53:41 +0600

    explore for loop

---
+++ PHP Loops/01_for_loop.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+    PHP for loop:
+     Is used when you know exactly how many times you want to iterate through a block of code. It consists of three expressions:
+            Initialization: 
+            Condition: 
+            Increment/Decrement:
+    syntax:
+        for ( Initialization; Condition; Increment/Decrement )
+         {
+            Code to be executed
+         }
+
+*/      //print number from 1 to 5.
+         for($i=1; $i<=5; $i++){
+            echo "$i\n";
+         }
+        
+?>
