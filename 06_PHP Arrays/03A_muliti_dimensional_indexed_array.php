f48006b867bc1d0f27dac5a76c6c6621a08a3eff
Amdad92 <emdadctg92@gmail.com>
2025-01-27 12:47:58 +0600

    study multidimensional indexed array

--- 06_PHP Arrays/03A_muliti_dimensional_indexed_array.php
+++ 06_PHP Arrays/03A_muliti_dimensional_indexed_array.php
@@ -1,6 +1,6 @@
 <?php 
  // Indexed Multidimensional Array
-$matrix = [
+ $matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
@@ -14,3 +14,41 @@ echo $matrix[1][2]."\n"; //2nd array 3nd item
 echo $matrix[2][0]."\n"; //3rd array first item 
 echo $matrix[2][1]."\n"; //3rd array 2nd item
 echo $matrix[2][2]."\n"; //3rd array 3rd item
+
+echo "----------------\n";
+
+//Access these array element using foreach
+
+foreach($matrix as $row){
+    foreach($row as $item){
+        echo $item." ";
+    }
+  echo PHP_EOL;
+}
+echo "----------------\n";
+
+// Example-2: Calculate the sum of total matrix value;
+
+$matrix2 = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+];
+
+foreach($matrix2 as $row){
+    foreach($row as $item){
+        echo $item." ";
+    }
+  echo PHP_EOL;
+}
+echo "----------------\n";
+
+$totalSumOfMatrix2=0;
+
+foreach($matrix2 as $row){
+    foreach($row as $element){
+        $totalSumOfMatrix2 += $element;
+    }
+}
+echo "The sum of total matrix value is: ".$totalSumOfMatrix2;
