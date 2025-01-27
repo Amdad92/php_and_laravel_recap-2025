b3e896ba43d4ea9d68e79b8284a39ce49e18edae
Amdad92 <emdadctg92@gmail.com>
2025-01-27 12:16:19 +0600

    study multidimensional array

---
+++ 06_PHP Arrays/03B_muliti_dimensional_array.php
@@ -0,0 +1,16 @@
+<?php 
+ // Indexed Multidimensional Array
+$matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+echo $matrix[0][0]."\n"; //first array first item
+echo $matrix[0][1]."\n"; //first array second item
+echo $matrix[0][2]."\n"; //first array 3rd item
+echo $matrix[1][0]."\n"; //2nd array first item
+echo $matrix[1][1]."\n"; //2nd array 2nd item
+echo $matrix[1][2]."\n"; //2nd array 3nd item
+echo $matrix[2][0]."\n"; //3rd array first item 
+echo $matrix[2][1]."\n"; //3rd array 2nd item
+echo $matrix[2][2]."\n"; //3rd array 3rd item
