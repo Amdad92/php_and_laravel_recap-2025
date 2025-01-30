fa39855b7a840ef83740730666c0a2dbe7e36a86
Amdad92 <emdadctg92@gmail.com>
2025-01-30 09:12:03 +0600

    study built in function in php

---
+++ 07_PHP Functions/06_built-in-php-functions.php
@@ -0,0 +1,18 @@
+<?php
+/*
+    PHP comes with many built-in functions that you can use without defining them. For example:
+
+    - strlen(): Returns the length of a string.
+
+    - date(): Returns the current date.
+
+    - array_push(): Adds an element to an array.
+*/
+
+// Example: Using Built-in Functions
+
+$text = "Hello, PHP!";
+echo "Length of text: " . strlen($text)."\n"; // Outputs: 11
+
+$today = date("d-m-Y");
+echo "Today's date: $today";
