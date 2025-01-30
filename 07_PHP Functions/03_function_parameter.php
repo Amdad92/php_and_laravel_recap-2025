29dacd39916eb0180d8c7b1baa0fe38541162321
Amdad92 <emdadctg92@gmail.com>
2025-01-30 08:39:55 +0600

    study function parameter

---
+++ 07_PHP Functions/03_function_parameter.php
@@ -0,0 +1,9 @@
+<?php
+/*
+    Functions can accept inputs called parameters. These are variables that you pass to the function when calling it. 
+*/
+function greet($name){
+    echo "Hello, $name! Welcome to PHP function".PHP_EOL;
+}
+greet("Amdadul Islam");
+greet("Dr. Sushmita Hoque");
