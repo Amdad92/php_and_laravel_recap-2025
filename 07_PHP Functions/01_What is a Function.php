a407f99bda9df9f3f2f13b20dc3be437b17b3e10
Amdad92 <emdadctg92@gmail.com>
2025-01-30 08:31:24 +0600

    Learn PHP functions

---
+++ 07_PHP Functions/01_What is a Function.php
@@ -0,0 +1,20 @@
+<?php 
+ /*
+    A function is a block of code that performs a specific task. It is designed to be reusable, meaning you can call the function multiple times without rewriting the same code. Functions help you organize your code, make it easier to read, and reduce errors.
+
+    Why Use Functions? 
+    - Reusability: Write once, use many times.
+
+    - Modularity: Break down complex tasks into smaller, manageable parts.
+
+    - Maintainability: Easier to debug and update code.
+
+    - Readability: Makes your code cleaner and easier to understand.
+
+    The basic syntax of a PHP function is:
+
+        function functionName(parameter1, parameter2, ...) {
+        Code to execute
+        return $result; // Optional
+        }
+ */
