2341c5ac0e592d4ff434407fed3974e6673f5ad2
Amdad92 <emdadctg92@gmail.com>
2025-01-30 13:02:17 +0600

    know about PHP files

---
+++ 09_working_with_files_PHP/01_Introduction_to_PHP_and_Files.php
@@ -0,0 +1,7 @@
+<?php 
+/*
+    - PHP is a powerful language for web development, and one of its key features is the ability to interact with files on the server. 
+
+    - Files are used to store data, such as user information, logs, or configuration settings. PHP provides built-in functions to handle files easily.
+    
+*/
